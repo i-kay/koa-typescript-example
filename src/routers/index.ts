c37b5db99efab485adbf9f2a505256664ff09230
황규순 (Kay) <hgs0426@gmail.com>
2019-10-06 22:22:28 +0900

    auth middleware 적용

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,4 +1,5 @@
 import * as Router from 'koa-router';
+import * as jwt from 'koa-jwt';
 
 // resource 하나의 routes. method에 대한 관심은 여기서 두지 않게 한다.
 import helloWorld from './lottos';
@@ -7,6 +8,7 @@ import auth from './auth';
 const router = new Router();
 
 router.use(auth.path, auth.router.routes());
+router.use(jwt({ secret: 'privateKey' }));
 router.use(helloWorld.path, helloWorld.router.routes());
 
 export default router;
