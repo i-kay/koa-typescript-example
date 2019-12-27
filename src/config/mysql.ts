848acd75657ff6987e7ea6b1415872fd736f4d64
황규순 (Kay) <hgs0426@gmail.com>
2019-12-27 18:07:34 +0900

    config 파일 수정

---
+++ src/config/mysql.ts
@@ -0,0 +1,7 @@
+export const mysql = {
+    MYSQL_HOST: process.env.MYSQL_HOST,
+    MYSQL_USER: process.env.MYSQL_USER,
+    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+};
+
+Object.entries(mysql).forEach(([key, value]) => {});
---
+++ src/config/server.ts
@@ -0,0 +1,4 @@
+export const server = {
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT,
+};
