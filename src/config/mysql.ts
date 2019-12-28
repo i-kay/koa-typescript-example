48be9f140b68b464c8768facdb8e3d3eb7e6eb73
황규순 (Kay) <hgs0426@gmail.com>
2019-12-28 22:56:05 +0900

    mysqldump로 초기 data 입력되도록 작업
    mysql docker container가 compose up 될 때, 초기 data가 입력되도록 작업 함

--- src/config/mysql.ts
+++ src/config/mysql.ts
@@ -2,6 +2,7 @@ export const mysql = {
     MYSQL_HOST: process.env.MYSQL_HOST,
     MYSQL_USER: process.env.MYSQL_USER,
     MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+    MYSQL_DATABASE: process.env.MYSQL_DATABASE,
 };
 
 Object.entries(mysql).forEach(([key, value]) => {
--- src/libs/mysqlClient/index.ts
+++ src/libs/mysqlClient/index.ts
@@ -5,7 +5,7 @@ const mysqlClient = mysql.createConnection({
     host: config.MYSQL_HOST,
     user: config.MYSQL_USER,
     password: config.MYSQL_PASSWORD,
-    database: '',
+    database: config.MYSQL_DATABASE,
 });
 
 mysqlClient.connect();
