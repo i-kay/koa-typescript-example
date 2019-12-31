bd88ee9d61919b83b3e262fa660d870b23f4f0d8
황규순 (Kay) <hgs0426@gmail.com>
2019-12-31 18:48:54 +0900

    타입 추가

--- src/libs/mysql-client/index.ts
+++ src/libs/mysql-client/index.ts
@@ -1,8 +1,9 @@
 import * as mysql from 'mysql2/promise';
 
 import { mysql as config } from '../../config';
+import { Connection } from 'mysql2/promise';
 
-export const getConn = async () => {
+export const getConn: () => Promise<Connection> = async () => {
     const conn = await mysql.createConnection({
         host: config.MYSQL_HOST,
         user: config.MYSQL_USER,
