5fa39c49640150306304d4594630873ac05884e2
황규순 (Kay) <hgs0426@gmail.com>
2020-01-01 23:48:09 +0900

    mysql2모듈의 import하는 방식 수정

--- src/libs/mysql-client/index.ts
+++ src/libs/mysql-client/index.ts
@@ -1,10 +1,9 @@
-import * as mysql from 'mysql2/promise';
+import { Connection, createConnection } from 'mysql2/promise';
 
 import { mysql as config } from '../../config';
-import { Connection } from 'mysql2/promise';
 
 export const getConn: () => Promise<Connection> = async () => {
-    const conn = await mysql.createConnection({
+    const conn = await createConnection({
         host: config.MYSQL_HOST,
         user: config.MYSQL_USER,
         password: config.MYSQL_PASSWORD,
