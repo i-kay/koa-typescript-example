628bc826efeb2a704480750b7e3d51cb50f699a4
황규순 (Kay) <hgs0426@gmail.com>
2020-01-05 10:37:42 +0900

    도메인 로직을 도메인 서비스로 옮김

--- src/applications/lotto/lotto.service.ts
+++ src/applications/lotto/lotto.service.ts
@@ -1,20 +1,15 @@
 import { notFound } from '@hapi/boom';
 
-import { Datetime } from '../../domains/common-types/datetime.type';
-
-import { Lotto } from '../../domains/lotto/lotto.model';
-import { User } from '../../domains/user/user.model';
-import { LottoNumber } from '../../domains/lotto/lottoNumber.model';
 import { UserId } from '../../domains/user/user.types';
 import { GameId } from '../../domains/game/game.types';
+import { PurchaseId } from '../../domains/lotto/lotto.types';
 import { LottoRepository } from '../../infrastructures/lotto/lotto.repository';
-import { UserRepository } from '../../infrastructures/user/user.repository';
 import { getConn } from '../../libs/mysql-client';
 import {
     PurchasedLottos,
     LottoViewPurchaseService,
 } from '../../domains/lotto/services/lotto.view-purchase.service';
-import { PurchaseId } from '../../domains/lotto/lotto.types';
+import { LottoCreationService } from '../../domains/lotto/services/lotto.creation.service';
 
 interface GetLottosByUserId {
     userId: UserId;
@@ -47,35 +42,17 @@ export class LottoService {
         gameId: GameId,
         lottoNumbersList: number[][],
     ) {
+        const lottoCreationService = new LottoCreationService();
         const dbConn = await getConn();
-        const user: User = await new UserRepository(dbConn).findById(userId);
 
-        if (!user) {
-            dbConn.end();
-            // TODO: 에러 메시지 정의 해야함
-            // 등록된 User가 아니면 예외처리
-            throw notFound();
-        }
+        await lottoCreationService.checkValidationBeforeCreated(dbConn, userId);
 
-        let lottos: Lotto[] = [];
-
-        lottoNumbersList.forEach((lottoNumbers, idx) => {
-            const lotto: Lotto = new Lotto({
-                purchaseId,
-                gameId,
-                userId,
-                order: idx + 1,
-                lottoNumbers: [
-                    new LottoNumber({ number: lottoNumbers[0] }),
-                    new LottoNumber({ number: lottoNumbers[1] }),
-                    new LottoNumber({ number: lottoNumbers[2] }),
-                    new LottoNumber({ number: lottoNumbers[3] }),
-                    new LottoNumber({ number: lottoNumbers[4] }),
-                    new LottoNumber({ number: lottoNumbers[5] }),
-                ],
-            });
-            lottos.push(lotto);
-        });
+        const lottos = lottoCreationService.processLottoNumbersList(
+            purchaseId,
+            userId,
+            gameId,
+            lottoNumbersList,
+        );
 
         // 생성된 lotto의 id
         const lottoIdList = await new LottoRepository(dbConn).save(lottos);
